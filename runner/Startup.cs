dceba187d715e95f54c3677afb99c96165a46c1c
Sandip Banodha <sandip.banodha@bcssarl.com>
2016-10-19 10:55:24 +0530

    initial commit

---
+++ runner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(runner.Startup))]
+namespace runner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
