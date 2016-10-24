a40779a28410e164f24aab228c32472116dd04ec
Sandip Banodha <sandip.banodha@bcssarl.com>
2016-10-24 11:08:51 +0530

    Add Employee Controller

---
+++ DemoWebAPI/DemoWebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoWebAPI.MvcApplication" Language="C#" %>
---
+++ runner/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace runner.Models
+{
+    public class Employee
+    {
+        public int EmpID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+}
